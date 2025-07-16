f8efc87074e210edfaa345d02389ecccbe2e68f4
Sachin Jindal <sachin121jindal@gmail.com>
2025-07-16 16:01:06 +0530

    Updated the code to use react query

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -7,17 +7,14 @@ import {
   Chip,
   Typography,
 } from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
   activity: Activity;
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
 };
-export default function ActivityCard({
-  activity,
-  selectActivity,
-  deleteActivity,
-}: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+  const { deleteActivity } = useActivities();
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardContent>
@@ -46,11 +43,12 @@ export default function ActivityCard({
           </Button>
           <Button
             onClick={() => {
-              deleteActivity(activity.id);
+              deleteActivity.mutate(activity.id);
             }}
             size="medium"
             color="error"
             variant="contained"
+            disabled={deleteActivity.isPending}
           >
             Delete
           </Button>
--- client/src/features/activities/dashboard/ActiviyList.tsx
+++ client/src/features/activities/dashboard/ActiviyList.tsx
@@ -3,14 +3,9 @@ import ActivityCard from "./ActivityCard";
 type Props = {
   activities: Activity[];
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
 };
 
-export default function ActiviyList({
-  activities,
-  selectActivity,
-  deleteActivity,
-}: Props) {
+export default function ActiviyList({ activities, selectActivity }: Props) {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {activities.map((activity) => (
@@ -18,7 +13,6 @@ export default function ActiviyList({
           key={activity.id}
           activity={activity}
           selectActivity={selectActivity}
-          deleteActivity={deleteActivity}
         />
       ))}
     </Box>
