f086c262ad0b097a042a4f8e84832bdcb03d648c
Sachin Jindal <sachin121jindal@gmail.com>
2025-07-25 14:42:10 +0530

    Edit profile feature

---
+++ client/src/lib/schemas/editProfileSchema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+import { requiredString } from "../utils/utils";
+export const editProfileSchema = z.object({
+  displayName: requiredString("Display Name"),
+  bio: z.string().optional(),
+});
+export type EditProfileSchema = z.infer<typeof editProfileSchema>;
